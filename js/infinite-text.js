3d12e7ecfca90a0ff9e8cba72144f742bb3b2008
Fraser <fraserchapman16@gmail.com>
2021-09-23 20:46:48 +0100

    initial commit of html and some source code

---
+++ js/infinite-text.js
@@ -0,0 +1,7 @@
+const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "."];
+
+
+function getRandomChar() {
+    const randomIndex = Math.round(Math.random() * (characters.length - 1));
+    return characters[randomIndex];
+}
