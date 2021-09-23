8ef1bf831dcdba958d2be0f5090d579ee266312f
Fraser <fraserchapman16@gmail.com>
2021-09-23 20:53:20 +0100

    add functionality to generate random text by manually clicking button per char

--- js/infinite-text.js
+++ js/infinite-text.js
@@ -1,5 +1,8 @@
 const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "."];
 
+function appendCharToOutput(char) {
+    document.getElementById("monkey-output").innerHTML += char;
+}
 
 function getRandomChar() {
     const randomIndex = Math.round(Math.random() * (characters.length - 1));
