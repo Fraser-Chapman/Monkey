7242b6ac05339f27f2234f317109b6d978a4fddc
Fraser <fraserchapman16@gmail.com>
2021-09-25 18:22:34 +0100

    remove unneccessary variable

--- js/infinite-text.js
+++ js/infinite-text.js
@@ -1,11 +1,9 @@
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '.'];
 const waitTimeBetweenExecutionsInMS = 100;
 
-var monkeyTyping = false;
 var intervalId;
 
 function startTyping() {
-    monkeyTyping = true;
     intervalId = setInterval(function () {appendCharToOutput(getRandomChar())}, waitTimeBetweenExecutionsInMS);
 }
 
@@ -24,7 +22,6 @@ function scrollToBottom() {
 }
 
 function stopTyping() {
-    monkeyTyping = false;
     clearInterval(intervalId);
 }
 
